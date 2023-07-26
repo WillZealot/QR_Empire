e7628987cd9a5f413707d234985d7e5e8e172a58
Tyler Underwood <twood9600@yahoo.com>
2023-07-26 19:59:51 -0400

    created the user id in url seed

--- seeds/url-seed.js
+++ seeds/url-seed.js
@@ -1,8 +1,16 @@
-const urlSeed = [
-    'https://www.google.com/',
-    'https://www.bing.com/',
-    'https://www.yahoo.com/',
+const urlSeed = [{
+   url:'https://www.google.com/',
+   userId: "1",
+},
+{
+   url:'https://www.bing.com/',
+   userId: "2",
+},
+{
+    url:'https://www.yahoo.com/',
+    userId: "3"
     // Add more URLs as needed
+}
   ];
   
   module.exports = urlSeed;
