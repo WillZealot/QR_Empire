bba50d186961d9b6f736d5e217efe57f406d7b97
Mav <ckendall626@gmail.com>
2023-07-26 19:45:30 -0400

    Added router to API and requisite API files.

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const userRoute = require('./user-route.js');
+const signUpRoute = require('./signup.js');
+const dashboardRoute = require('./dashboard.js');
+const logInRoute = require('./login-route.js');
+const viewUserRoute = require('./viewUser-route.js');
+
+router.use('/user', userRoute);
+router.use('/signUp', signUpRoute);
+router.use('/dashboard', dashboardRoute);
+router.use('/logIn', logInRoute);
+router.use('/viewUser', viewUserRoute);
+
+module.exports = router;
