31a4a58d8ffb68b2b829fa810541b91b92fccb82
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-07-26 20:18:23 -0400

    adding code for url-seed.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const seedUsers = require('./user-seed');
+const seedURL = require('./url-seed')
 
 
 const sequelize = require('../config/connection');
@@ -6,7 +7,7 @@ const sequelize = require('../config/connection');
 const seedAll = async() => {
     await sequelize.sync({ force: true });
     await seedUsers();
-   
+    await seedURL();
     process.exit(0);
 };
 
