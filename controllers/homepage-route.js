ab3d653943171356adcd3dfe6f9aab6b16f9d5ad
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-08-01 15:59:57 -0400

    fixed some css and modified some code for layout

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const { User, QRLink } = require('../models');
-// Import the custom middleware
 const withAuth = require('../utils/auth');
 
-// GET all post for homepage
 router.get('/', async (req, res) => {
   try {
     res.status(200).render('homepage')
@@ -31,7 +29,6 @@ router.get('/signup', async (req, res) => {
 
 router.get('/dashboard',withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: QRLink }],
