e482555241ee084d421edc5f110962c34bb557a9
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-08-02 19:27:14 -0400

    reverted the file

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,22 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+// connection to database through sequelize goes here
+const Sequelize = require('sequelize')
+require('dotenv').config()
 
-let sequelize;
+let sequelize
 
 if (process.env.JAWSDB_URL) {
-  // Use JAWSDB_URL for the database connection on Heroku
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
-  // Use local database configuration for development
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  )
 }
 
-module.exports = sequelize;
+module.exports = sequelize
