05f33cb427ea5987750f96c1871e144468d4cae4
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-08-02 19:12:00 -0400

    fixed some errors

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homepage-route.js');
+const apiRoutes = require('./api')
+const homeRoutes = require('./homepage-route.js')
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use('/', homeRoutes)
+router.use('/api', apiRoutes)
 
-module.exports = router;
+module.exports = router
