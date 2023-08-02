c4b2b58e1edb84de12ba3d8c22b2fbecc81c6b48
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-08-01 20:26:11 -0400

    added some more styles and removed a few thingsd from pages also encountered an error

--- seeds/url-seed.js
+++ seeds/url-seed.js
@@ -1,20 +1,6 @@
 const { QRLink } = require('../models');
 
 const urlSeed = [{
-   description:'https://www.google.com/',
-   user_id: 1,
-},
-{
-   description:'https://www.bing.com/',
-   user_id: 2,
-},
-{
-   description:'https://www.yahoo.com/',
-   user_id: 3
-},
-{
-   description:'https://www.apple.com/',
-   user_id: 3
 }
   ];
   
